50c1389b1ed2e95227c64fac235264f3fdded3a0
assem <gddfggggdadg12@gmail.com>
2024-07-10 16:30:02 +0300

    first commit

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let isConnected = false; // track the connection
+let isConnected = false; // Variable to track the connection status
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
+  // Set strict query mode for Mongoose to prevent unknown field queries.
+  mongoose.set("strictQuery", true);
 
-  if(isConnected) {
-    console.log('MongoDB is already connected');
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
+
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log("MongoDB connection already established");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
+    });
 
-    isConnected = true;
-
-    console.log('MongoDB connected')
+    isConnected = true; // Set the connection status to true
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
   }
-}
+};
